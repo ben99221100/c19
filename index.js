33a3f71b53caa4413dbec4a50b30ac80b4948e91
ben99221100 <134168955+ben99221100@users.noreply.github.com>
2023-06-27 21:31:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// let code = prompt("הכנס קוד:")
+
+// while (code != "abc123") {
+//     code = prompt("קוד שגוי, נסה שוב:")
+// }
+
+
+let taskElement = document.getElementById("taskId")
+let container = document.getElementById("container")
+let tasks = []
+
+function add() {
+    tasks.push(taskElement.value)
+
+    container.innerHTML = ""
+
+    for(let i=0; i < tasks.length; i++) {
+        container.innerHTML += `<p> ${tasks[i]} </p>`  
+    }
+
+    taskElement.value = ""
+
+}
+
+function updateBackground() {
+    let colorElement = document.getElementById("colorId")
+    document.body.style.backgroundColor = colorElement.value
+}
